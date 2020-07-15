3a1ac071acc8d570479ddd69fcde3ddfe529735e
Sander <svkatwijk@gmail.com>
2020-07-15 11:43:20 +0200

    Used toeplitz Z matrix to speed up FiniteArray
    - Exploited the toeplitz nature of the mutual impedance matrix to reduce number of integrals in kx.

--- Elements/FiniteArray.m
+++ Elements/FiniteArray.m
@@ -8,8 +8,9 @@ classdef FiniteArray < handle
         zfeed % Impedance of the feeding network
         
         Zmat % Mutual impedance matrix
+        Z_fs % Frequencies for impedance matrix
         
-        Ds % Precomputed Ds, contains 1/sum(D(nypp,kx))
+        Ds    % Precomputed Ds
         D_kxs % Kx values for precomputed Ds
         D_fs  % Frequencies for precomputed Ds
     end
@@ -23,12 +24,14 @@ classdef FiniteArray < handle
             
         end
         function InitializeZMatrix(this, fs)
+            % Ensure the appropriate D integrals have been calculated.
             if(isempty(this.Ds) || ~isempty(setdiff(fs, this.D_fs)))
-                this.PrecomputeDs(fs);
+                this.InitializeDs(fs);
             end
             tc = tic;
             
-            Nf = length(fs);
+            newfs = setdiff(fs, this.Z_fs);
+            Nf = length(newfs);
             Nx_ = this.Nx;
             Ny_ = this.Ny;
             dedge_ = this.dedge;
@@ -37,24 +40,18 @@ classdef FiniteArray < handle
             dy = this.unitcell.dy;
             wslot = this.unitcell.wslot;
             dslot = this.unitcell.dslot;
-            tlineup = this.unitcell.tlineup;
-            tlinedown = this.unitcell.tlinedown;
-            walled = this.unitcell.walled;
             
-            z0 = Constants.z0;
             c0 = Constants.c0;
-            
-%             this = parallel.pool.Constant(this);
                 
             %% Initialize progress bar
             hDataQueue = parallel.pool.DataQueue;
             hWaitbar = waitbar(0, {'0% Initializing...', '', ''});
             afterEach(hDataQueue, @updateWaitbar);
             
-            Zmat_ = zeros((Nx_+2)*Ny_, (Nx_+2)*Ny_, Nf);
-            global fi;
+%             Zmat_ = zeros((Nx_+2)*Ny_, (Nx_+2)*Ny_, Nf);
+%             global fi;
             for(fi = 1:Nf)
-                f = fs(fi);
+                f = newfs(fi);
 %                 dispex('%i / %i.\n', fi, length(fs));
                 
                 k0 = 2*pi*f/c0;
@@ -63,11 +60,13 @@ classdef FiniteArray < handle
                 g = 5/3 * sqrt(wslot * lambda);
                 
                 Ffeed = @(kx) sinc(kx .* dslot ./ (2*pi));
-                Fedge = @(kx) ( besselj(0, kx .* g./2) - 1j .* struve(0, kx.*g./2) ...
+                Fright = @(kx) ( besselj(0, kx .* g./2) - 1j .* struve(0, kx.*g./2) ...
                                 - 2/pi .* sinc(kx.*g./(4*pi)).*exp(-1j.*kx.*g./4)) .* exp(1j.*kx.*g./2);
                 % The basis function of the second termination is the reverse of that of the first
                 % termination.
-                Fedge2 = @(kx) Fedge(-kx); 
+                Fleft = @(kx) Fright(-kx); 
+                
+                
                 
                 %% Integration limits & path
                 % Go to +-inf
@@ -78,22 +77,18 @@ classdef FiniteArray < handle
                 integrationpath = [(-1-1j).*delta, (1+1j).*delta];
                 
                 %% Set up indexing.
-                [nxs, nys, nxps, nyps] = ndgrid(-1:Nx_, 0:Ny_-1, -1:Nx_, 0:Ny_-1);
-                nxs = nxs(:);
-                nys = nys(:);
-                nxps = nxps(:);
-                nyps = nyps(:);
+                [nxs, nys, nxps, nyps] = Z_Indexing(Nx_, Ny_);
                 
                 N = length(nxs);
                 progress = -1; send(hDataQueue, nan);
                 
-                Z = zeros(1, N);
+                Zvec = zeros(1, N);
                 
                 xs = [-dedge_, (0:Nx_-1)*dx, (Nx_-1)*dx+dedge_];
                 Fs = cell(1,Nx_+2);
-                Fs{1} = Fedge;
+                Fs{1} = Fleft;
                 Fs(2:Nx_+1) = {Ffeed};
-                Fs{end} = Fedge2;
+                Fs{end} = Fright;
                 
                 parfor(in = 1:N)
                     nx = nxs(in);
@@ -110,21 +105,22 @@ classdef FiniteArray < handle
                     F = Fs{nx+2}; %#ok<PFBNS> "The entire array or structure 'Fs' is a broadcast variable."
                     Fp = Fs{nxp+2};
                     
+                    
 %                     dispex('nx %i, ny %i, nxp %i, nyp %i\n   x %f, xp %f\n', nx, ny, nxp, nyp, x, xp);
                     
                     z = -dy./(2*pi).* ...
                         integral(...
                             @(kx) Z_Integrand_kx(this, f, dy, kx, Ny_, ny, nyp, x, xp, F, Fp), ...
                             lim1, lim2, 'Waypoints', integrationpath);
-                    Z(in) = z;
+                    Zvec(in) = z;
                     
                     send(hDataQueue, nan); % Update progress bar
                 end
-                % Reshape Z such that the rows are the (nx, ny) combinations, and the columns are
-                % the (nxp, nyp) for those indices.
-                Zmat_(:,:,fi) = reshape(Z, (Nx_+2)*Ny_, []);
+                % Reshape the Z vector into the correct matrix, exploiting the block-toeplitz
+                % structure of the matrix.
+                this.Zmat{end+1} = Z_Matrix(Zvec, Nx_, Ny_);
+                this.Z_fs(end+1) = f;
             end
-            this.Zmat = Zmat_;
             
             function updateWaitbar(~)
                 progress = progress + 1;
@@ -135,9 +131,11 @@ classdef FiniteArray < handle
             end
             delete(hWaitbar);
             
-            toc(tc);
+            dt = toc(tc);
+            dispex('Z matrix took %.1fs for %ix%i elements, %i frequencies.\n', dt, Nx_, Ny_, Nf);
         end
-        function PrecomputeDs(this, fs)
+        function InitializeDs(this, fs)
+            tc = tic;
             Nf = length(fs);
             Ny_ = this.Ny;
             
@@ -206,11 +204,18 @@ classdef FiniteArray < handle
                      sprintf('%i/%i kxs done.', progress, N)});
             end
             delete(hWaitbar);
+            dt = toc(tc);
+            dispex('D took %.1fs for %i slots, %i frequencies.\n', dt, Ny_, Nf);
         end
         function Zas = GetInputImpedance(this, fs, excitation)
             if(size(excitation, 1) ~= this.Nx || size(excitation, 2) ~= this.Ny)
                 error('Invalid excitation matrix supplied. Should be Nx by Ny.');
             end
+            % Ensure the appropriate Z matrices have been calculated.
+            if(isempty(this.Zmat) || ~isempty(setdiff(fs, this.Z_fs)))
+                this.InitializeZMatrix(fs);
+            end
+            tc = tic;
             
             Nf = length(fs);
             Nx_ = this.Nx;
@@ -221,9 +226,12 @@ classdef FiniteArray < handle
             excitation_ = zeros(Nx_+2, Ny_);
             excitation_(2:end-1, :) = excitation;
             
-            Zas = zeros((Nx_+2)*Ny_, length(fs));
-            for(fi = 1:length(fs))
-                Zmat_ = this.Zmat(:, :, fi);
+            Zas = zeros((Nx_+2)*Ny_, Nf);
+            for(fi = 1:Nf)
+                f = fs(fi);
+                % Retrieve the Z matrix for this frequency
+                fii = find(this.Z_fs == f, 1);
+                Zmat_ = this.Zmat{fii};
                 % Build a matrix with zfeed on the diagonal, but only for non-termination elements
                 Zl = repmat([0, ones(1, Nx_)*zfeed_, 0], 1, Ny_);
                 Zlmat = diag(Zl);
@@ -239,7 +247,148 @@ classdef FiniteArray < handle
             
             % Reshape the output matrix and drop the edge impedances.
             Zas = reshape(Zas, Nx_+2, Ny_, Nf);
-%             Zas = Zas(2:end-1, :, :); % Don't drop yet, for debugging
+            Zas = Zas(2:end-1, :, :);
+
+            dt = toc(tc);
+            dispex('Active Z took %.1fms for %ix%i elements, %i frequencies.\n', dt*1e3, Nx_, Ny_, Nf);
+        end
+        function BuildCST(this, project)
+            this.unitcell.BuildCST(project);
+            
+            project.StoreParameter('lambda_min', 'c0/fmin/1e9');
+            project.StoreParameter('Nx', num2str(this.Nx, '%.15g'));
+            project.StoreParameter('Ny', num2str(this.Ny, '%.15g'));
+            project.StoreParameter('edge_distance', num2str(this.dedge*1e3, '%.15g'));
+            project.StoreParameter('edge_length', '5/3 * sqr(slot_width * lambda_min)');
+            project.StoreParameter('padding_x', 'max(0, lambda_min/4-edge_length)');
+            project.StoreParameter('padding_y', 'max(0, lambda_min/4-(dy/2-slot_width/2))');
+            
+            transform = project.Transform();
+            brick = project.Brick();
+            solid = project.Solid();
+            
+            %% Copy the unit cell in the x-direction
+            % Only if there's more than 1 feed in X.
+            project.NextCommandConditional('Nx > 1');
+                transform.Reset();
+                transform.Name('Slot');
+                transform.Vector('dx', 0, 0);
+                transform.MultipleObjects(1);
+                transform.GroupObjects(1);
+                transform.Repetitions('Nx-1');
+                transform.Transform('Shape', 'Translate');
+            
+            %% Create the terminations
+            brick.Reset();
+            brick.Name('Termination1');
+            brick.Component('Slot');
+            brick.Xrange('-edge_distance-edge_length',  '-dx/2');
+            brick.Yrange('-dy/2', 'dy/2');
+            brick.Zrange('0', '0');
+            brick.Material('PEC');
+            brick.Create();
+            
+            brick.Reset();
+            brick.Name('Termination1_Slot');
+            brick.Component('Slot');
+            brick.Xrange('-edge_distance',  '-dx/2');
+            brick.Yrange('-slot_width/2', 'slot_width/2');
+            brick.Zrange('0', '0');
+            brick.Material('PEC');
+            brick.Create();
+            
+            brick.Reset();
+            brick.Name('Termination2');
+            brick.Component('Slot');
+            brick.Xrange('(Nx-1)*dx+dx/2',  '(Nx-1)*dx+edge_distance+edge_length');
+            brick.Yrange('-dy/2', 'dy/2');
+            brick.Zrange('0', '0');
+            brick.Material('PEC');
+            brick.Create();
+            
+            brick.Reset();
+            brick.Name('Termination2_Slot');
+            brick.Component('Slot');
+            brick.Xrange('(Nx-1)*dx+dx/2',  '(Nx-1)*dx+edge_distance');
+            brick.Yrange('-slot_width/2', 'slot_width/2');
+            brick.Zrange('0', '0');
+            brick.Material('PEC');
+            brick.Create();
+            
+            solid.Subtract('Slot:Termination1', 'Slot:Termination1_Slot');
+            solid.Subtract('Slot:Termination2', 'Slot:Termination2_Slot');
+            
+            %% Copy the slot in the y-direction
+            % Only if there's more than 1 slot in Y.
+            project.NextCommandConditional('Ny > 1');
+                transform.Reset();
+                transform.Name('Slot');
+                transform.Vector(0, 'dy', 0);
+                transform.MultipleObjects(1);
+                transform.GroupObjects(1);
+                transform.Repetitions('Ny-1');
+                transform.Transform('Shape', 'Translate');
+            
+            
+            %% Copy the port in the x-direction
+            % Only if there's more than 1 feed in X.
+            project.NextCommandConditional('Nx > 1');
+                transform.Reset();
+                transform.Name('port1 (SlotFeed)');
+                transform.Vector('dx', 0, 0);
+                transform.MultipleObjects(1);
+                transform.Repetitions('Nx-1');
+                transform.Transform('Port', 'Translate');
+
+            %% Copy the port in the x-direction
+            % Only if there's more than 1 slot in Y.
+            project.NextCommandConditional('Ny > 1');
+                % Copy the port of each nx.
+                project.NextCommandLoop('nxi', '1', 'Nx')
+                    transform.Reset();
+                    transform.Name('port" & nxi & " (SlotFeed)');
+                    transform.Vector(0, 'dy', 0);
+                    transform.MultipleObjects(1);
+                    transform.Repetitions('Ny-1');
+                    transform.Transform('Port', 'Translate');
+            
+            %% Add the padding to ensure lambda/4 spacing from the slot to the open boundary
+            project.NextCommandConditional('padding_x > 0');
+                brick.Reset();
+                brick.Name('Padding_x1');
+                brick.Component('Slot');
+                brick.Xrange('-edge_distance-edge_length-padding_x',  '-edge_distance-edge_length');
+                brick.Yrange('-dy/2', '(Ny-1)*dy+dy/2');
+                brick.Zrange('0', '0');
+                brick.Material('PEC');
+                brick.Create();
+            project.NextCommandConditional('padding_x > 0');
+                brick.Reset();
+                brick.Name('Padding_x2');
+                brick.Component('Slot');
+                brick.Xrange('(Nx-1)*dx+edge_distance+edge_length',  '(Nx-1)*dx+edge_distance+edge_length+padding_x');
+                brick.Yrange('-dy/2', '(Ny-1)*dy+dy/2');
+                brick.Zrange('0', '0');
+                brick.Material('PEC');
+                brick.Create();
+            project.NextCommandConditional('padding_y > 0');
+                brick.Reset();
+                brick.Name('Padding_y1');
+                brick.Component('Slot');
+                brick.Xrange('-edge_distance-edge_length-padding_x', '(Nx-1)*dx+edge_distance+edge_length+padding_x');
+                brick.Yrange('-dy/2-padding_y',  '-dy/2');
+                brick.Zrange('0', '0');
+                brick.Material('PEC');
+                brick.Create();
+            project.NextCommandConditional('padding_y > 0');
+                brick.Reset();
+                brick.Name('Padding_y2');
+                brick.Component('Slot');
+                brick.Xrange('-edge_distance-edge_length-padding_x', '(Nx-1)*dx+edge_distance+edge_length+padding_x');
+                brick.Yrange('(Ny-1)*dy+dy/2',  '(Ny-1)*dy+dy/2+padding_y');
+                brick.Zrange('0', '0');
+                brick.Material('PEC');
+                brick.Create();
         end
     end
 end
@@ -261,81 +410,12 @@ function v = Z_Integrand_kx(this, f, dy, kx, Ny_, ny, nyp, x, xp, F, Fp)
         end
     end
     v = int2 .* Fp(kx) .* F(-kx) .* exp(-1j .* kx .* (x-xp));
-    return
-    %%
-    sum(v) %#ok<UNRCH>
-    pltx = kx;
-    plty = D./(2*pi);
-    [hFig, hAx] = figureex;
-        title(hAx, 'D./(2*pi)');
-        hAx.ColorOrder = lines(min(size(plty)));
-        plot(real(pltx), real(plty));
-        plot(real(pltx), imag(plty), '--');
-        plot(real(pltx), abs(plty), ':');
-        alignplot(hFig, 5, 3, [], hFig.Number, 1);
-    pltx = kx;
-    plty = int2;
-    [hFig, hAx] = figureex;
-        title(hAx, 'int2');
-        hAx.ColorOrder = lines(min(size(plty)));
-        plot(real(pltx), real(plty));
-        plot(real(pltx), imag(plty), '--');
-        plot(real(pltx), abs(plty), ':');
-        alignplot(hFig, 5, 3, [], hFig.Number, 1);
-    pltx = kx;
-    plty = v;
-    [hFig, hAx] = figureex;
-        title(hAx, 'v');
-        hAx.ColorOrder = lines(min(size(plty)));
-        plot(real(pltx), real(plty));
-        plot(real(pltx), imag(plty), '--');
-        plot(real(pltx), abs(plty), ':');
-        alignplot(hFig, 5, 3, [], hFig.Number, 1);
 end
 
 function v = Z_Integrand_ky(ky, Ny_, ny, nyp, dy, D)
-%     sumval = 0;
-%     for(nypp = 0:Ny_-1)
-%         sumval = sumval + D(nypp+1) .* exp(1j .* ky .* nypp .* dy);
-%     end
-
     nypp = (0:Ny_-1).';
     sumval = sum(D.' .* exp(1j .* (nypp * ky)  .* dy), 1);
-    v = exp(-1j .* ky .* abs(ny-nyp) .* dy) ./ sumval;
-    
-%     sumval = sum(D.', 1);
-%     v = ones(size(ky)) ./ sumval;
-
-    return;
-    %%
-    sum(v) %#ok<UNRCH>
-    pltx = ky;
-    plty = D.' .* exp(1j .* (nypp * ky)  .* dy);
-    [hFig, hAx] = figureex;
-        title(hAx, 'D.'' .* exp(1j .* (nypp * ky)  .* dy)');
-        hAx.ColorOrder = lines(min(size(plty)));
-        plot(real(pltx), real(plty));
-        plot(real(pltx), imag(plty), '--');
-        plot(real(pltx), abs(plty), ':');
-        alignplot(hFig, 5, 3, [], hFig.Number, 1);
-    pltx = ky;
-    plty = sumval;
-    [hFig, hAx] = figureex;
-        title(hAx, 'sumval');
-        hAx.ColorOrder = lines(min(size(plty)));
-        plot(real(pltx), real(plty));
-        plot(real(pltx), imag(plty), '--');
-        plot(real(pltx), abs(plty), ':');
-        alignplot(hFig, 5, 3, [], hFig.Number, 1);
-    pltx = ky;
-    plty = v;
-    [hFig, hAx] = figureex;
-        title(hAx, 'v');
-        hAx.ColorOrder = lines(min(size(plty)));
-        plot(real(pltx), real(plty));
-        plot(real(pltx), imag(plty), '--');
-        plot(real(pltx), abs(plty), ':');
-        alignplot(hFig, 5, 3, [], hFig.Number, 1);
+    v = exp(-1j .* ky .* (ny-nyp) .* dy) ./ sumval;
 end
 
 function v = D_Integrand_kyp(f, dy, k0, kyp, kx, tlineup, tlinedown, z0, wslot, nypp)
@@ -363,30 +443,52 @@ function v = D_Integrand_kyp(f, dy, k0, kyp, kx, tlineup, tlinedown, z0, wslot, 
 
     Gxx = Gxxup + Gxxdown;
 
-    v = Gxx.*besselj(0, kyp.*wslot./2).*exp(+1j.*kyp.*(-nypp).*dy);
-    return
-    %%
-    sum(v)
-    pltx = kyp;
-    plty = v;
-    [hFig, hAx] = figureex;
-        title(hAx, 'v');
-        hAx.ColorOrder = lines(min(size(plty)));
-        plot(real(pltx), real(plty));
-        plot(real(pltx), imag(plty), '--');
-        plot(real(pltx), abs(plty), ':');
-        alignplot(hFig, 5, 3, [], hFig.Number, 1);
+    v = Gxx.*besselj(0, kyp.*wslot./2).*exp(-1j.*kyp.*nypp.*dy);
+end
+
+function [nxs, nys, nxps, nyps] = Z_Indexing(Nx_, Ny_)
+    % Interaction of ny=0:Ny_-1 and nyp=0 % All slots
+        % nx=-1:Nx_ and nxp=-1, edge to all
+        %                                               nx,      ny,      nxp, nyp
+        [nxs_mat, nys_mat, nxps_mat, nyps_mat] = ndgrid(-1:Nx_,  0:Ny_-1, -1,  0);
+        nxs = nxs_mat(:);
+        nys = nys_mat(:);
+        nxps = nxps_mat(:);
+        nyps = nyps_mat(:);
+        % nx=0:Nx_-1 and nxp=0, feed to feeds
+        %                                               nx,      ny,      nxp, nyp
+        [nxs_mat, nys_mat, nxps_mat, nyps_mat] = ndgrid(0:Nx_-1, 0:Ny_-1, 0,   0);
+        nxs = [nxs; nxs_mat(:)];
+        nys = [nys; nys_mat(:)];
+        nxps = [nxps; nxps_mat(:)];
+        nyps = [nyps; nyps_mat(:)];
+end
+
+function Zmat = Z_Matrix(Zvec, Nx_, Ny_)
+    % Generate the blocks
+    blocks = cell(1,Ny_);
+    for(ny = 0:Ny_-1)
+        % First the Zvec has Nx_+2 elements which give the coupling between -1 to nx (edge to all)
+        % This repeats Ny_ times
+        ind = (1:Nx_+2)+ny*(Nx_+2);
+        block = toeplitz(real(Zvec(ind)))+1j .* toeplitz(imag(Zvec(ind)));
+        % Then we have the the coupling between 0 to nx (feed to feed)
+        ind = (1:Nx_) + Ny_*(Nx_+2) + ny*Nx_;
+        block(2:end-1, 2:end-1) = toeplitz(real(Zvec(ind)))+1j .* toeplitz(imag(Zvec(ind)));
+        blocks{ny+1} = block;
+    end
+    
+    Zmat = cell2mat(blocks(toeplitz(1:Ny_)));
 end
 
 
 
-
-
-
-
-
-
-
-
-
-
+%     pltx = kx;
+%     plty = v;
+%     [hFig, hAx] = figureex;
+%         title(hAx, 'v');
+%         hAx.ColorOrder = lines(min(size(plty)));
+%         plot(real(pltx), real(plty));
+%         plot(real(pltx), imag(plty), '--');
+%         plot(real(pltx), abs(plty), ':');
+%         alignplot(hFig, 5, 3, [], hFig.Number, 1);
